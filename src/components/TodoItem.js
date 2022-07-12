88ad956c4acab2f870c2f5ab5854df7251f8f150
Elisha Tetteyfio <93135004+Elisha-Tetteyfio@users.noreply.github.com>
2022-07-12 09:30:36 +0000

    Part 2 - React form and handling events

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -3,7 +3,17 @@ import React from "react"
 class TodoItem extends React.Component {
   render() {
     return(
-     <li>{this.props.todo.title}</li>
+      <li>
+        <input 
+          type="checkBox" 
+          checked={this.props.todo.completed}
+          onChange={() => this.props.handleChangeProps(this.props.todo.id)}
+        /> 
+        <button onClick={() => this.props.deleteTodoProps(this.props.todo.id)}>
+          delete
+        </button>
+        {this.props.todo.title}
+      </li>
     )
   }
 }
